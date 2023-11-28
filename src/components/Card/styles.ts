b80b911e411c91e5ef2a6cba8dd96a9f907bbf6d
augustouser1 <augustocsbitencourt8@gmail.com>
2023-11-28 16:51:54 -0300

    comentário

---
+++ src/components/Card/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const Aside = styled.aside`
+  background-color:#FDCD6B;
+  border-radius: 1rem;
+  padding: 1rem;
+  line-height: 2rem;
+  h3 strong{
+    font-weight: bold;
+  }
+`
